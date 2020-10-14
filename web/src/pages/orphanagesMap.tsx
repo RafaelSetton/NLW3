07ae5cbd3115be65ffcafe4be6996fe0766a2954
Rafael Setton <rasealca2017@gmail.com>
2020-10-13 22:05:16 -0400

    first commit

---
+++ web/src/pages/orphanagesMap.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import 'leaflet/dist/leaflet.css';
+import '../styles/pages/orphanages-map.css';
+
+function OrphanagesMap() {
+    return (
+        <div id="page-map">
+          <aside>
+            <header>
+              <img src={mapMarkerImg} alt="Happy"/>
+              <h2>Escolha um orfanato no mapa</h2>
+              <p>Muitas crianças estão esperando a sua visita</p>
+            </header>
+
+            <footer>
+              <strong>Porto Velho</strong>
+              <span>Rondônia</span>
+            </footer>
+          </aside>
+
+          <Map 
+            center={[-8.7581297, -63.8793798]}
+            zoom={15}
+            style={{width: '100%', height: '100%'}}
+          >
+            <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          </Map>
+
+          <Link to="" className="create-orphanage" >
+            <FiPlus size={32} color="#fff" />
+          </Link>
+        </div>
+      );
+}
+
+export default OrphanagesMap;
